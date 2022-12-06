2ff7ee2c2aa4f99873ab2d5351feadf7ad8aa750
sione-tenifa <johntenifa@yahoo.com>
2022-12-05 20:33:24 -0700

    working app

--- pages/search.js
+++ pages/search.js
@@ -4,9 +4,9 @@ import Image from 'next/image'
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
-import Property from '../components/Property';
-import SearchFilters from '../components/SearchFilters';
-import { baseUrl, fetchApi } from '../utils/fetchApi';
+import Property from '../components/property';
+import SearchFilters from '../components/searchfilters';
+import { baseUrl, fetchApi } from '../utils/fetchapi';
 import noresult from '../assets/images/noresult.jpg'
 
 const Search = ({ properties }) => {
